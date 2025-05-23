f6b605b6106f1b0b99132aaaf780378719c3f92a
Pol Gorrindo Bermejo <polgorrindo@gmail.com>
2025-05-23 14:00:56 +0200

    No wofi, no nautilus, yes thunar, yer rofi

--- basic.sh
+++ basic.sh
@@ -1,3 +1,19 @@
-hyprpaper zsh oh-my-zsh rofi waybar
-sudo pacman -S ttf-jetbrains-mono-nerd-font
-sudo apt install fonts-jetbrains-mono-nerd-font
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -S hyprpaper zsh base-devel git thefuck rofi waybar ttf-jetbrains-mono-nerd net-tools bat exa --noconfirm
+sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
+source ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/ohmyzsh/ohmyzsh/tree/master/themes/heapbytes.zsh-theme $ZSH_CUSTOM/themes/heapbytes
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+rm -rf yay
+sudo pacman -R wofi
+
+
