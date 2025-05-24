511791c397b6f6d7df74a3cfe51f7ad98768767d
polete <polgorrindo@gmail.com>
2025-05-24 21:08:19 +0200

    Oh-my-zsh añadido

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-position = $(pwd)
+position=$(pwd)
 
 sudo pacman -Syu --noconfirm
-sudo pacman -S wireplumber hyprpaper base-devel git thefuck rofi waybar ttf-jetbrains-mono-nerd net-tools adw-gtk-theme qt5ct qt6ct breeze-icons bat exa thunar --noconfirm
-
-cp -r config/* ~/.config/
-cp -r .nanorc ~/.nanorc
-
-hyprpaper -c ~/hypr/hyprpaper.conf
+sudo pacman -S zsh wireplumber hyprpaper base-devel git thefuck rofi waybar ttf-jetbrains-mono-nerd net-tools adw-gtk-theme qt5ct qt6ct breeze-icons bat exa thunar --noconfirm
 
 cd ~
 
@@ -17,23 +12,25 @@ cd yay
 makepkg -si
 cd ~
 
-rm -rf yay 
+rm -rf yay
 sudo pacman -R wofi vim --noconfirm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-s
-yntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosugge
-stions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 git clone https://github.com/heapbytes/heapbytes-zsh.git
 mv heapbytes-zsh/heapbytes.zsh-theme ~/.oh-my-zsh/themes/heapbytes.zsh-theme
 rm -rf heapbytes-zsh
 
-cp $position/.zshrc ~/.zshrc
+cp -r $position/config/* ~/.config/
+cp -r $position/.nanorc ~/.nanorc
+cp -r $position/.zshrc ~/.zshrc
+cp -r $position/.oh-my-zsh ~/.oh-my-zsh
 clear
 echo 'To set a wallpaper just type "set_wallpaper wallpaper.png"'
 echo 'It is recommended to reboot now.'
 
+hyprpaper -c ~/hypr/hyprpaper.conf
